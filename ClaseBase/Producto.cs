d45eef2b3ca384a91c4278ef741f8716540cd352
fsr1410 <faq.ramos.14101999@gmail.com>
2022-06-10 18:19:19 -0300

    Agregado de las clases intervinientes

---
+++ ClaseBase/Producto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClaseBase
+{
+    public class Producto
+    {
+        private int prod_Codigo;
+
+        public int Prod_Codigo
+        {
+            get { return prod_Codigo; }
+            set { prod_Codigo = value; }
+        }
+        private string prod_Categoria;
+
+        public string Prod_Categoria
+        {
+            get { return prod_Categoria; }
+            set { prod_Categoria = value; }
+        }
+        private string prod_Descripcion;
+
+        public string Prod_Descripcion
+        {
+            get { return prod_Descripcion; }
+            set { prod_Descripcion = value; }
+        }
+        private Decimal prod_Precio;
+
+        public Decimal Prod_Precio
+        {
+            get { return prod_Precio; }
+            set { prod_Precio = value; }
+        }
+    }
+}
---
+++ ClaseBase/Roles.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClaseBase
+{
+    public class Roles
+    {
+        private int rol_Codigo;
+
+        public int Rol_Codigo
+        {
+            get { return rol_Codigo; }
+            set { rol_Codigo = value; }
+        }
+        private string rol_Descripcion;
+
+        public string Rol_Descripcion
+        {
+            get { return rol_Descripcion; }
+            set { rol_Descripcion = value; }
+        }
+
+        public Roles(int codigo, string descripcion)
+        {
+            rol_Codigo = codigo;
+            rol_Descripcion = descripcion;
+        }
+    }
+}
